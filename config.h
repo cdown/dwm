83724283b1ff09d0ee56eccb612ff0bf86f98143
Chris Down <chris@chrisdown.name>
2017-01-06 22:19:34 +0000

    config: Disable bar

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Terminus:size=8" };
 static const char dmenufont[]       = "Terminus:size=8";
