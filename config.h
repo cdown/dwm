230ca5dc81c7bda7a49bebe940a0e1cfddcaa761
Chris Down <chris@chrisdown.name>
2017-01-06 22:40:29 +0000

    config: Disable resize hints

--- config.h
+++ config.h
@@ -46,7 +46,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.5;  /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
