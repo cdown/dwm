e5b4b1add1573270cf25c11790703bba15117c89
Chris Down <chris@chrisdown.name>
2020-04-15 01:55:03 +0100

    Try to debug unreliable launching

--- dwm.c
+++ dwm.c
@@ -1134,6 +1134,7 @@ keypress(XEvent *e)
 
 	ev = &e->xkey;
 	keysym = XKeycodeToKeysym(dpy, (KeyCode)ev->keycode, 0);
+	fprintf(stderr, "got keycode %s (%lu)\n", XKeysymToString(keysym), keysym);
 	for (i = 0; i < LENGTH(keys); i++)
 		if (keysym == keys[i].keysym
 		&& ev->type == keys[i].type
@@ -1607,6 +1608,8 @@ run(void)
 	while (running && !XNextEvent(dpy, &ev))
 		if (handler[ev.type])
 			handler[ev.type](&ev); /* call handler */
+		else
+			fprintf(stderr, "no handler for type %d\n", ev.type);
 }
 
 void
@@ -1873,6 +1876,7 @@ spawn(const Arg *arg)
 {
 	if (arg->v == dmenucmd)
 		dmenumon[0] = '0' + selmon->num;
+	fprintf(stderr, "arg->v: %s\n", *(char **)arg->v);
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
