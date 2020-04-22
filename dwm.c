7bad00407f37ae548a3b3f489886227a3b671ec3
Chris Down <chris@chrisdown.name>
2020-04-22 14:45:08 +0100

    warp: Client isn't const, leave it alone

--- dwm.c
+++ dwm.c
@@ -244,7 +244,7 @@ static void updatetitle(Client *c);
 static void updatewindowtype(Client *c);
 static void updatewmhints(Client *c);
 static void view(const Arg *arg);
-static void warp(const Client *c);
+static void warp(Client *c);
 static Client *wintoclient(Window w);
 static Monitor *wintomon(Window w);
 static int xerror(Display *dpy, XErrorEvent *ee);
@@ -2340,7 +2340,7 @@ view(const Arg *arg)
 }
 
 void
-warp(const Client *c)
+warp(Client *c)
 {
 	int x, y;
 
@@ -2349,7 +2349,7 @@ warp(const Client *c)
 		return;
 	}
 
-	Atom wtype = getatomprop((Client *)c, netatom[NetWMWindowType]);
+	Atom wtype = getatomprop(c, netatom[NetWMWindowType]);
 	if (wtype == netatom[NetWMWindowTypeDialog] ||
 	    wtype == netatom[NetWMWindowTypeNotification])
 		return;
