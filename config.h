908563fd0687925947158fcc6263f821600fd14b
Chris Down <chris@chrisdown.name>
2021-03-29 11:44:51 +0000

    config: Don't force terminal tag

--- config.h
+++ config.h
@@ -37,7 +37,6 @@ static const Rule rules[] = {
 	{ NULL,       NULL,       "Scratch",  0,            True,       True,        -1 },
 	{ NULL,       NULL,       "mail",     1 << 3,       False,      False,       -1 },
 	{ NULL,       NULL,       "irc",      1 << 2,       False,      False,       -1 },
-	{ NULL,       NULL,       "Terminal", 1,            False,      False,       -1 },
 	{ NULL,       NULL,       pp_moni,    0,            False,      True,       2  },
 	{ NULL,       NULL,       pp_proj,    0,            False,      True,       1  },
 };
