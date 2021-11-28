6e88e96028434f931a4b91a1d79d39e43fc3aed5
Chris Down <chris@chrisdown.name>
2021-11-28 00:32:08 +0000

    config: Add lockfullscreen

--- config.h
+++ config.h
@@ -45,6 +45,7 @@ static const Rule rules[] = {
 static const float mfact     = 0.5;  /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const LayoutConfigRule lc_rules[] = {
 	/* Uses default nmaster/mfact
