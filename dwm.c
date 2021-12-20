2bbcf4b30c556bfdda44d68563984a28a3cb88ab
Chris Down <chris@chrisdown.name>
2021-12-20 13:17:39 +0000

    unmanage: Windows may move, so do focus() last

--- dwm.c
+++ dwm.c
@@ -2203,9 +2203,9 @@ unmanage(Client *c, int destroyed)
 		XUngrabServer(dpy);
 	}
 	free(c);
-	focus(getclientundermouse());
 	updateclientlist();
 	arrange(m);
+	focus(getclientundermouse());
 }
 
 void
