f7dd781a556fc7cb6096d21f0f54deede11b1f94
Chris Down <chris@chrisdown.name>
2021-12-20 13:23:19 +0000

    warp: Still warp even if inside window
    I already relaxed these earlier somewhat in "warp: Still warp even if
    the pointer is in the border", but I really don't see the point of these
    checks in general. If warp() gets called, we performed some explicit
    action which warrants warping, so having it centered seems reasonable.

--- dwm.c
+++ dwm.c
@@ -2487,8 +2487,6 @@ view(const Arg *arg)
 void
 warp(Client *c)
 {
-	int x, y;
-
 	/* Try to warp to the active window. */
 	if (!c)
 		c = selmon->sel;
@@ -2498,13 +2496,6 @@ warp(Client *c)
 		XWarpPointer(dpy, None, root, 0, 0, 0, 0, selmon->wx + selmon->ww/2, selmon->wy + selmon->wh/2);
 		return;
 	}
-
-	if (!getrootptr(&x, &y) ||
-	    (x > c->x &&
-	     y > c->y &&
-	     x < c->x + c->w &&
-	     y < c->y + c->h))
-		return;
 
 	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w / 2, c->h / 2);
 }
