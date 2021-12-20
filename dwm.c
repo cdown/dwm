bb39819eaf77995ef64f7b8c21bf8bc5dca5e48f
Chris Down <chris@chrisdown.name>
2021-12-20 13:05:43 +0000

    Only warp on manage(), no need to do it for all map requests

--- dwm.c
+++ dwm.c
@@ -1375,6 +1375,7 @@ manage(Window w, XWindowAttributes *wa)
 	arrange(c->mon);
 	XMapWindow(dpy, c->win);
 	focus(NULL);
+	warp(c);
 }
 
 void
@@ -1397,8 +1398,6 @@ maprequest(XEvent *e)
 		return;
 	if (!wintoclient(ev->window))
 		manage(ev->window, &wa);
-
-	warp(wintoclient(ev->window));
 }
 
 void
