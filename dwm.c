9b116a4b1098b779e91614c9b168154b9139b49f
Chris Down <chris@chrisdown.name>
2021-12-20 19:31:06 +0000

    layouts: Remove bstackhoriz
    Never used.

--- dwm.c
+++ dwm.c
@@ -163,7 +163,6 @@ static void attach(Client *c);
 static void attachaside(Client *c);
 static void attachstack(Client *c);
 static void bstack(Monitor *m);
-static void bstackhoriz(Monitor *m);
 static void buttonpress(XEvent *e);
 static void checkotherwm(void);
 static void cleanup(void);
@@ -529,36 +528,6 @@ bstack(Monitor *m) {
 			resize(c, tx, ty, tw - (2 * c->bw), h - (2 * c->bw), 0);
 			if (tw != m->ww)
 				tx += WIDTH(c);
-		}
-	}
-}
-
-static void
-bstackhoriz(Monitor *m) {
-	int w, mh, mx, tx, ty, th;
-	unsigned int i, n;
-	Client *c;
-
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
-		return;
-	if (n > m->nmaster) {
-		mh = m->nmaster ? m->mfact * m->wh : 0;
-		th = (m->wh - mh) / (n - m->nmaster);
-		ty = m->wy + mh;
-	} else {
-		th = mh = m->wh;
-		ty = m->wy;
-	}
-	for (i = mx = 0, tx = m->wx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-		if (i < m->nmaster) {
-			w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
-			resize(c, m->wx + mx, m->wy, w - (2 * c->bw), mh - (2 * c->bw), 0);
-			mx += WIDTH(c);
-		} else {
-			resize(c, tx, ty, m->ww - (2 * c->bw), th - (2 * c->bw), 0);
-			if (th != m->wh)
-				ty += HEIGHT(c);
 		}
 	}
 }
