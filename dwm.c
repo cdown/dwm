17a71bd0523f2f145f2ab891b14a2ceaf08cbf6c
Chris Down <chris@chrisdown.name>
2022-03-17 13:25:09 +0000

    manage: propertynotify: Reduce cost of unused size hint updates
    This patch defers all size hint calculations until they are actually
    needed, drastically reducing the number of calls to updatesizehints(),
    which can be expensive when called repeatedly (as it currently is during
    resizes).
    
    In my unscientific testing this reduces calls to updatesizehints() by
    over 90% during a typical work session. There are no functional changes
    for users other than an increase in responsiveness after resizes and
    reduction in CPU time.
    
    There are two main motivations to defer this work to the time of hint
    application:
    
    1. Some clients, especially terminals using incremental size hints,
       resend XA_WM_NORMAL_HINTS on resize to avoid fighting with the WM or
       mouse resizing. For example, some terminals like urxvt clear
       PBaseSize and PResizeInc during XResizeWindow and restore them
       afterwards.
    
       For this reason we typically receive a backlogged XA_WM_NORMAL_HINTS
       message for each update period with movement, which is useless. In
       some cases one may get hundreds or thousands of XA_WM_NORMAL_HINTS
       messages on large resizes, and currently all of these result in a
       separate updatesizehints() call which is immediately invalidated.
    
       (We can't just blindly discard these messages during resizes like we
       do for EnterNotify, because some of them might actually be for other
       windows, or not be XA_WM_NORMAL_HINTS events.)
    
    2. For users using resizehints=0 most of these updates are unused anyway
       -- in the normal case where the client is not floating these values
       won't be used, so there's no need to calculate them up front.
    
    A synthetic test using the mouse to resize a floating terminal window
    from roughly 256x256 to 1024x1024 and back again shows that the number
    of calls to updatesizehints() goes from over 500 (one for each update
    interval with movement) to 2 (one for each hint application), with no
    change in user visible behaviour.
    
    This also reduces the delay before dwm is ready to process new events
    again after a large resize on such a client, as it avoids the thundering
    herd of updatesizehints() calls when hundreds of backlogged
    XA_WM_NORMAL_HINTS messages appear at once after a resize is finished.
    In slower environments or X servers, this improvement in responsiveness
    is often tangible after resizing a client that changes hints during
    resizes.

--- dwm.c
+++ dwm.c
@@ -89,7 +89,7 @@ struct Client {
 	float mina, maxa;
 	int x, y, w, h;
 	int oldx, oldy, oldw, oldh;
-	int basew, baseh, incw, inch, maxw, maxh, minw, minh;
+	int basew, baseh, incw, inch, maxw, maxh, minw, minh, hintsvalid;
 	int bw, oldbw;
 	unsigned int tags;
 	int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen;
@@ -345,6 +345,8 @@ applysizehints(Client *c, int *x, int *y, int *w, int *h, int interact)
 	if (*w < bh)
 		*w = bh;
 	if (resizehints || c->isfloating || !c->mon->lt[c->mon->sellt]->arrange) {
+		if (!c->hintsvalid)
+			updatesizehints(c);
 		/* see last two sentences in ICCCM 4.1.2.3 */
 		baseismin = c->basew == c->minw && c->baseh == c->minh;
 		if (!baseismin) { /* temporarily remove base dimensions */
@@ -1059,7 +1061,6 @@ manage(Window w, XWindowAttributes *wa)
 	XSetWindowBorder(dpy, w, scheme[SchemeNorm][ColBorder].pixel);
 	configure(c); /* propagates border_width, if size doesn't change */
 	updatewindowtype(c);
-	updatesizehints(c);
 	updatewmhints(c);
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
@@ -1233,7 +1234,7 @@ propertynotify(XEvent *e)
 				arrange(c->mon);
 			break;
 		case XA_WM_NORMAL_HINTS:
-			updatesizehints(c);
+			c->hintsvalid = 0;
 			break;
 		case XA_WM_HINTS:
 			updatewmhints(c);
@@ -1989,6 +1990,7 @@ updatesizehints(Client *c)
 	} else
 		c->maxa = c->mina = 0.0;
 	c->isfixed = (c->maxw && c->maxh && c->maxw == c->minw && c->maxh == c->minh);
+	c->hintsvalid = 1;
 }
 
 void
