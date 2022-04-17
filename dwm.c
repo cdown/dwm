c81326b0fdb9922a9b536d6d21c96b5f5d1fa651
Chris Down <chris@chrisdown.name>
2022-04-17 13:17:57 +0200

    Hide border if only one client is visible

--- dwm.c
+++ dwm.c
@@ -1650,6 +1650,15 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	if ((nexttiled(c->mon->clients) == c) && !(nexttiled(c->next)))
 		resetlayout(NULL);
 
+	if (((nexttiled(c->mon->clients) == c && !nexttiled(c->next))
+	    || &monocle == c->mon->lt[c->mon->sellt]->arrange)
+	    && !c->isfullscreen && !c->isfloating
+	    && NULL != c->mon->lt[c->mon->sellt]->arrange) {
+		c->w = wc.width += c->bw * 2;
+		c->h = wc.height += c->bw * 2;
+		wc.border_width = 0;
+	}
+
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
 	configure(c);
 	XSync(dpy, False);
