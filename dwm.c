a4771de5ba54a38b062a7d748635f21c141b5c7e
Hiltjo Posthuma <hiltjo@codemadness.org>
2022-04-26 10:30:59 +0200

    Revert "manage: For isfloating/oldstate check/set, ensure trans client actually exists"
    This reverts commit bece862a0fc4fc18ef9065b18cd28e2032d0d975.
    
    It caused a regression, for example:
    https://lists.suckless.org/hackers/2203/18220.html

--- dwm.c
+++ dwm.c
@@ -1065,7 +1065,7 @@ manage(Window w, XWindowAttributes *wa)
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
 	if (!c->isfloating)
-		c->isfloating = c->oldstate = t || c->isfixed;
+		c->isfloating = c->oldstate = trans != None || c->isfixed;
 	if (c->isfloating)
 		XRaiseWindow(dpy, c->win);
 	attach(c);
