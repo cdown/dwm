ab9b5cd8fc1f0a6c79f8d33f8bb979236cd5df5a
Chris Down <chris@chrisdown.name>
2022-10-17 04:44:45 +0100

    Remove some old fprintfs for passive debugging

--- dwm.c
+++ dwm.c
@@ -1234,7 +1234,6 @@ keypress(XEvent *e)
 
 	ev = &e->xkey;
 	keysym = XKeycodeToKeysym(dpy, (KeyCode)ev->keycode, 0);
-	fprintf(stderr, "got keycode %s (%lu)\n", XKeysymToString(keysym), keysym);
 	for (i = 0; i < LENGTH(keys); i++)
 		if (keysym == keys[i].keysym
 		&& ev->type == keys[i].type
@@ -1727,8 +1726,6 @@ run(void)
 	while (running && !XNextEvent(dpy, &ev))
 		if (handler[ev.type])
 			handler[ev.type](&ev); /* call handler */
-		else
-			fprintf(stderr, "no handler for type %d\n", ev.type);
 }
 
 void
@@ -1998,7 +1995,6 @@ spawn(const Arg *arg)
 
 	if (arg->v == dmenucmd)
 		dmenumon[0] = '0' + selmon->num;
-	fprintf(stderr, "arg->v: %s\n", *(char **)arg->v);
 	if (fork() == 0) {
 		char *const *cmd = arg->v;
 		if (dpy)
