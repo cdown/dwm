ba56fe9fea0a28d8184a727a987836a0903e2682
Hiltjo Posthuma <hiltjo@codemadness.org>
2022-10-28 16:37:56 +0200

    Revert "Remove dmenumon variable"
    This reverts commit c2b748e7931e5f28984efc236f9b1a212dbc65e8.
    
    Revert back this change. It seems to not be an edge-case anymore since
    multiple users have asked about this new behaviour now.

--- config.def.h
+++ config.def.h
@@ -56,7 +56,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
