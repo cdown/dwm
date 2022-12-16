7c843f3bcc1b55ab366c4a69188c27bdc3425d87
Chris Down <chris@chrisdown.name>
2022-12-16 00:59:58 +0000

    asan

--- config.mk
+++ config.mk
@@ -23,12 +23,12 @@ FREETYPEINC = /usr/include/freetype2
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -fsanitize=address
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -march=native -mtune=native -std=c99 -pedantic -Wall -Wno-unused-function -Wno-deprecated-declarations -flto -O2 ${INCS} ${CPPFLAGS}
+CFLAGS   = -Og -ggdb -fno-omit-frame-pointer -fsanitize=address -std=c99 -pedantic -Wall ${INCS} ${CPPFLAGS}
+#CFLAGS   = -march=native -mtune=native -std=c99 -pedantic -Wall -Wno-unused-function -Wno-deprecated-declarations -flto -O2 ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
