c1e165d5fe962a7c1d0b962de63337e6de3bb95d
Chris Down <chris@chrisdown.name>
2023-04-17 18:29:14 +0100

    resizeclient: fakefullscreen: Fix growing issue
    c->bw is ignored and the border width is 0 when solitary(), so don't add
    it.

--- dwm.c
+++ dwm.c
@@ -1657,8 +1657,8 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	wc.border_width = c->bw;
 
 	if (solitary(c)) {
-		c->w = wc.width += c->bw * 2;
-		c->h = wc.height += c->bw * 2;
+		c->w = wc.width;
+		c->h = wc.height;
 		wc.border_width = 0;
 	}
 
