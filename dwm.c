d45075457e29e4a341c2afdd0b09791ed25e55a4
Chris Down <chris@chrisdown.name>
2023-05-19 04:10:33 +0100

    resizeclient: Fit to monitor on solitary
    The previous solution in "resizeclient: fakefullscreen: Fix growing
    issue" ignores the border width, but this results in unused pixels.
    
    We needed that because otherwise we grow as we fullscreen/unfullscreen
    since c->bw is never set to 0.
    
    Instead, just clamp to monitor width/height/x/y.

--- dwm.c
+++ dwm.c
@@ -1650,16 +1650,23 @@ resizeclient(Client *c, int x, int y, int w, int h)
 {
 	XWindowChanges wc;
 
-	c->oldx = c->x; c->x = wc.x = x;
-	c->oldy = c->y; c->y = wc.y = y;
-	c->oldw = c->w; c->w = wc.width = w;
-	c->oldh = c->h; c->h = wc.height = h;
-	wc.border_width = c->bw;
+	c->oldx = c->x;
+	c->oldy = c->y;
+	c->oldw = c->w;
+	c->oldh = c->h;
 
 	if (solitary(c)) {
-		c->w = wc.width;
-		c->h = wc.height;
-		wc.border_width = 0;
+		c->x = wc.x = c->mon->wx;
+		c->y = wc.y = c->mon->wy;
+		c->w = wc.width = c->mon->ww;
+		c->h = wc.height = c->mon->wh;
+		c->bw = wc.border_width = 0;
+	} else {
+		c->x = wc.x = x;
+		c->y = wc.y = y;
+		c->w = wc.width = w;
+		c->h = wc.height = h;
+		c->bw = wc.border_width = borderpx;
 	}
 
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
