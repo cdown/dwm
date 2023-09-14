7aed7a8908d045716c5ff551a8e4fe6d9af493c2
Chris Down <chris@chrisdown.name>
2023-09-14 19:44:12 +0100

    Revert "grabkeys: Only call from focus() if we need to ungrab"
    This doesn't rebind keys again when away from the window.
    
    This reverts commit 5823cfc1c20baefda7d07e40f486435df7a621e7.

--- dwm.c
+++ dwm.c
@@ -235,7 +235,7 @@ static void setlayout(const Arg *arg);
 static void setmfact(const Arg *arg);
 static void setup(void);
 static void seturgent(Client *c, int urg);
-static int shouldgrabkey(Client *c, Key *key);
+static int shouldgrabkey(Client *c, Key key);
 static void showhide(Client *c);
 static int solitary(Client *c);
 static void spawn(const Arg *arg);
@@ -1016,9 +1016,7 @@ focus(Client *c)
 		XDeleteProperty(dpy, root, netatom[NetActiveWindow]);
 	}
 	selmon->sel = c;
-	/* only needed if we just moved to a window with an ungrab rule */
-	if (!shouldgrabkey(c, NULL))
-		grabkeys();
+	grabkeys();
 	drawbars();
 }
 
@@ -1193,7 +1191,7 @@ grabkeys(void)
 			return;
 		for (k = start; k <= end; k++)
 			for (i = 0; i < LENGTH(keys); i++) {
-				if (!shouldgrabkey(selmon->sel, &keys[i]))
+				if (!shouldgrabkey(selmon->sel, keys[i]))
 					continue;
 				/* skip modifier codes, we do that ourselves */
 				if (keys[i].keysym == syms[(k - start) * skip])
@@ -2004,7 +2002,7 @@ seturgent(Client *c, int urg)
 
 /* 0: should not bind, 1: should bind. */
 int
-shouldgrabkey(Client *c, Key *key)
+shouldgrabkey(Client *c, Key key)
 {
 	unsigned int i;
 	const KeyRule *kr;
@@ -2015,9 +2013,8 @@ shouldgrabkey(Client *c, Key *key)
 	for (i = 0; i < LENGTH(keyrules); i++) {
 		kr = &keyrules[i];
 		if ((kr->title && strstr(c->name, kr->title)) &&
-		    (!key ||
-			((kr->mod == AnyModifier || kr->mod == key->mod) &&
-			 (kr->keysym == AnyKey || kr->keysym == key->keysym))))
+		    (kr->mod == AnyModifier || kr->mod == key.mod) &&
+		    (kr->keysym == AnyKey || kr->keysym == key.keysym))
 			return 0;
 	}
 
