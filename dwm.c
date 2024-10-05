8933ebcf50024f4378a78e556b1ac08091197206
Hiltjo Posthuma <hiltjo@codemadness.org>
2024-10-05 13:01:49 +0200

    sync drw.{c,h} from dmenu
    - drw: minor improvement to the nomatches cache
    - overhaul utf8decoding and render invalid utf8 sequences as U+FFFD.
    
    Thanks NRK for these improvements!

--- dwm.c
+++ dwm.c
@@ -50,7 +50,6 @@
 #define INTERSECT(x,y,w,h,m)    (MAX(0, MIN((x)+(w),(m)->wx+(m)->ww) - MAX((x),(m)->wx)) \
                                * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
 #define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]))
-#define LENGTH(X)               (sizeof X / sizeof X[0])
 #define MOUSEMASK               (BUTTONMASK|PointerMotionMask)
 #define WIDTH(X)                ((X)->w + 2 * (X)->bw)
 #define HEIGHT(X)               ((X)->h + 2 * (X)->bw)
